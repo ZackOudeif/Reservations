49af33ebea12477bcfbd960b013b3eaa5b2ba38c
Zack Oudeif <zackoudeif@Zacks-Mac-mini.local>
2025-02-06 11:10:00 -0500

    setting up init

---
+++ Reservations/Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by Zack Oudeif on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Reservations/ReservationsTests/ReservationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsTests.swift
+//  ReservationsTests
+//
+//  Created by Zack Oudeif on 2/6/25.
+//
+
+import Testing
+@testable import Reservations
+
+struct ReservationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
